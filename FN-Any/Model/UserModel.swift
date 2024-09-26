a18f57c44e0864df7b716bddaa7ba1d4bf687e30
kimlean <70122033+kimlean@users.noreply.github.com>
2024-09-26 09:51:12 +0700

    Update UserModel.swift
    Update readable code from API whichout any error

--- FN-Any/Model/UserModel.swift
+++ FN-Any/Model/UserModel.swift
@@ -1,7 +1,22 @@
 import Foundation
 
 struct User: Decodable {
-    let id: Int
+    let id: String
     let name: String
     let email: String
+
+     // Custom initializer to set a default value for isChecking
+    enum CodingKeys: String, CodingKey {
+        case id, name, email, isChecking, status
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        email = try container.decode(String.self, forKey: .email)
+        isChecking = (try? container.decode(Bool.self, forKey: .isChecking)) ?? false // Default to false if not present
+        isDelete = (try? container.decode(Bool.self, forKey: .isChecking)) ?? false
+        status = (try? container.decode(Int.self, forKey: .status)) ?? 0
+    }
 }
